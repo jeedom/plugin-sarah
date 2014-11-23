9bbbbb2df6fd7a5ce7bc0ca47f8a01594dab6437
Loic <loicgevrey@gmail.com>
2014-11-23 12:50:38 +0100

    Improve responsive design

--- desktop/php/sarah.php
+++ desktop/php/sarah.php
@@ -6,7 +6,7 @@ sendVarToJS('eqType', 'sarah');
 ?>
 
 <div class="row row-overflow">
-    <div class="col-lg-2">
+    <div class="col-lg-2 col-md-3 col-sm-4">
         <div class="bs-sidebar">
             <ul id="ul_eqLogic" class="nav nav-list bs-sidenav">
                 <a class="btn btn-default eqLogicAction" style="width : 100%;margin-top : 5px;margin-bottom: 5px;" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter une S.A.R.A.H}}</a>
@@ -19,20 +19,20 @@ sendVarToJS('eqType', 'sarah');
             </ul>
         </div>
     </div>
-    <div class="col-lg-10 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
+    <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
         <form class="form-horizontal">
             <fieldset>
                 <legend>{{Générale}}</legend>
                 <div class="form-group">
-                    <label class="col-lg-2 control-label">{{Nom de l'équipement S.A.R.A.H}}</label>
-                    <div class="col-lg-3">
+                    <label class="col-sm-2 control-label">{{Nom de l'équipement S.A.R.A.H}}</label>
+                    <div class="col-sm-3">
                         <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
                         <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement S.A.R.A.H}}"/>
                     </div>
                 </div>
                 <div class="form-group">
-                    <label class="col-lg-2 control-label" >{{Objet parent}}</label>
-                    <div class="col-lg-3">
+                    <label class="col-sm-2 control-label" >{{Objet parent}}</label>
+                    <div class="col-sm-3">
                         <select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
                             <option value="">{{Aucun}}</option>
                             <?php
@@ -44,21 +44,21 @@ sendVarToJS('eqType', 'sarah');
                     </div>
                 </div>
                 <div class="form-group">
-                    <label class="col-lg-2 control-label" >{{Activer}}</label>
-                    <div class="col-lg-1">
+                    <label class="col-sm-2 control-label" >{{Activer}}</label>
+                    <div class="col-sm-1">
                         <input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>
                     </div>
                 </div>
 
                 <div class="form-group">
-                    <label class="col-lg-2 control-label">{{Adresse du nodeJS}}</label>
-                    <div class="col-lg-3">
+                    <label class="col-sm-2 control-label">{{Adresse du nodeJS}}</label>
+                    <div class="col-sm-3">
                         <input type="text" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="addrSrv" placeholder="xxx.xxx.xxx.xxx:8080"/>
                     </div>
                 </div>
                 <div class="form-group">
-                    <label class="col-lg-2 control-label">{{Adresse du TTS}}</label>
-                    <div class="col-lg-3">
+                    <label class="col-sm-2 control-label">{{Adresse du TTS}}</label>
+                    <div class="col-sm-3">
                         <input type="text" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="addrSrvTts" placeholder="xxx.xxx.xxx.xxx:8888"/>
                     </div>
                 </div>
