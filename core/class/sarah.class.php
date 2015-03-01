ca0244795532cdb4de7cb4eedf1665364f77f2bb
Loic <loicgevrey@gmail.com>
2015-03-01 13:22:06 +0100

    Bugfix

--- core/class/sarah.class.php
+++ core/class/sarah.class.php
@@ -20,117 +20,114 @@
 require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
 
 class sarah extends eqLogic {
-    /*     * *************************Attributs****************************** */
+	/*     * *************************Attributs****************************** */
 
+	/*     * ***********************Methode static*************************** */
 
-    /*     * ***********************Methode static*************************** */
+	public static function generateXmlGrammar() {
+		$xmlWildcard = '';
+		$xml = "<grammar version=\"1.0\" xml:lang=\"fr-FR\" mode=\"voice\" root=\"ruleJeedom\" xmlns=\"http://www.w3.org/2001/06/grammar\" tag-format=\"semantics/1.0\">\r\n";
+		$xml .= "<rule id=\"ruleJeedom\" scope=\"public\">\r\n";
+		$xml .= "<tag>out.action=new Object(); </tag>\r\n";
+		$xml .= "<item>" . config::byKey('sarahName', 'sarah', 'Sarah') . "</item>\r\n";
+		$xml .= "<one-of>\r\n";
+		foreach (interactQuery::all() as $interactQuery) {
+			if ($interactQuery->getEnable() == 1) {
+				$query = $interactQuery->getQuery();
+				preg_match_all("/#(.*?)#/", $query, $matches);
+				$matches = $matches[1];
+				if (count($matches) > 0) {
+					$xmlWildcard .= "<rule id=\"ruleJeedom_" . $interactQuery->getId() . "\" scope=\"public\">\r\n";
+					$xmlWildcard .= "<tag>out.action=new Object();</tag>\r\n";
+					foreach ($matches as $match) {
+						$beforeMatch = substr($query, 0, strpos($query, "#" . $match . "#"));
+						$query = substr($query, strpos($query, "#" . $match . "#") + strlen("#" . $match . "#"));
+						$xmlWildcard .= "<item>" . $beforeMatch . "</item>\r\n";
+						$xmlWildcard .= "<ruleref special=\"GARBAGE\" />\r\n";
+					}
+					if (strlen($query) > 0) {
+						$xmlWildcard .= "<item>" . $query . "</item>\r\n";
+					}
+					$xmlWildcard .= "</rule>\r\n";
+					$xml .= "<item><ruleref uri=\"#ruleJeedom_" . $interactQuery->getId() . "\"/><tag>out._attributes.dictation=\"true\";out.action.id=\"" . $interactQuery->getId() . "\"; out.action.method=\"execute\"</tag></item>\r\n";
+				} else {
+					$xml .= "<item>" . $interactQuery->getQuery() . "<tag>out.action.id=\"" . $interactQuery->getId() . "\"; out.action.method=\"execute\"</tag></item>\r\n";
+				}
+			}
+		}
+		$xml .= "</one-of>\r\n";
+		$xml .= "<tag>out.action._attributes.uri=\"http://127.0.0.1:8080/sarah/jeedom\";</tag>\r\n";
+		$xml .= "</rule>\r\n";
+		$xml .= $xmlWildcard;
+		$xml .= "</grammar>\r\n";
+		return $xml;
+	}
 
-    public static function generateXmlGrammar() {
-        $xmlWildcard = '';
-        $xml = "<grammar version=\"1.0\" xml:lang=\"fr-FR\" mode=\"voice\" root=\"ruleJeedom\" xmlns=\"http://www.w3.org/2001/06/grammar\" tag-format=\"semantics/1.0\">\r\n";
-        $xml .= "<rule id=\"ruleJeedom\" scope=\"public\">\r\n";
-        $xml .= "<tag>out.action=new Object(); </tag>\r\n";
-        $xml .= "<item>" . config::byKey('sarahName', 'sarah', 'Sarah') . "</item>\r\n";
-        $xml .= "<one-of>\r\n";
-        foreach (interactQuery::all() as $interactQuery) {
-            if ($interactQuery->getEnable() == 1) {
-                $query = $interactQuery->getQuery();
-                preg_match_all("/#(.*?)#/", $query, $matches);
-                $matches = $matches[1];
-                if (count($matches) > 0) {
-                    $xmlWildcard .= "<rule id=\"ruleJeedom_" . $interactQuery->getId() . "\" scope=\"public\">\r\n";
-                    $xmlWildcard .= "<tag>out.action=new Object();</tag>\r\n";
-                    foreach ($matches as $match) {
-                        $beforeMatch = substr($query, 0, strpos($query, "#" . $match . "#"));
-                        $query = substr($query, strpos($query, "#" . $match . "#") + strlen("#" . $match . "#"));
-                        $xmlWildcard .= "<item>" . $beforeMatch . "</item>\r\n";
-                        $xmlWildcard .= "<ruleref special=\"GARBAGE\" />\r\n";
-                    }
-                    if (strlen($query) > 0) {
-                        $xmlWildcard .= "<item>" . $query . "</item>\r\n";
-                    }
-                    $xmlWildcard .= "</rule>\r\n";
-                    $xml .= "<item><ruleref uri=\"#ruleJeedom_" . $interactQuery->getId() . "\"/><tag>out._attributes.dictation=\"true\";out.action.id=\"" . $interactQuery->getId() . "\"; out.action.method=\"execute\"</tag></item>\r\n";
-                } else {
-                    $xml .= "<item>" . $interactQuery->getQuery() . "<tag>out.action.id=\"" . $interactQuery->getId() . "\"; out.action.method=\"execute\"</tag></item>\r\n";
-                }
-            }
-        }
-        $xml .= "</one-of>\r\n";
-        $xml .= "<tag>out.action._attributes.uri=\"http://127.0.0.1:8080/sarah/jeedom\";</tag>\r\n";
-        $xml .= "</rule>\r\n";
-        $xml .= $xmlWildcard;
-        $xml .= "</grammar>\r\n";
-        return $xml;
-    }
+	/*     * *********************Methode d'instance************************* */
 
-    /*     * *********************Methode d'instance************************* */
+	public function ping() {
+		$http = new com_http($this->getConfiguration('addrSrv') . '/sarah/jeedom?method=ping');
+		try {
+			$http->exec(1, 1, false);
+			return true;
+		} catch (Exception $e) {
+			return false;
+		}
+	}
 
-    public function ping() {
-        $http = new com_http($this->getConfiguration('addrSrv') . '/sarah/jeedom?method=ping');
-        try {
-            $http->exec(1, 1, false);
-            return true;
-        } catch (Exception $e) {
-            return false;
-        }
-    }
+	public function updateSrvSarah() {
+		$http = new com_http($this->getConfiguration('addrSrv') . '/sarah/jeedom?method=update');
+		$return = $http->exec(30);
+		if ($return != 'Mise à jour du xml réussi') {
+			throw new Exception($return);
+		}
+		return true;
+	}
 
-    public function updateSrvSarah() {
-        $http = new com_http($this->getConfiguration('addrSrv') . '/sarah/jeedom?method=update');
-        $return = $http->exec(30);
-        if ($return != 'Mise à jour du xml réussi') {
-            throw new Exception($return);
-        }
-        return true;
-    }
-
-    public function postSave() {
-        $sarahCmd = $this->getCmd(null, 'speak');
-        if (!is_object($sarahCmd)) {
-            $sarahCmd = new sarahCmd();
-        }
-        $sarahCmd->setName('Dit');
-        $sarahCmd->setLogicalId('speak');
-        $sarahCmd->setEqLogic_id($this->getId());
-        $sarahCmd->setType('action');
-        $sarahCmd->setSubType('message');
-        $sarahCmd->save();
-    }
-
-    public function dontRemoveCmd() {
-        return true;
-    }
+	public function postSave() {
+		$sarahCmd = $this->getCmd(null, 'speak');
+		if (!is_object($sarahCmd)) {
+			$sarahCmd = new sarahCmd();
+		}
+		$sarahCmd->setName('Dit');
+		$sarahCmd->setLogicalId('speak');
+		$sarahCmd->setEqLogic_id($this->getId());
+		$sarahCmd->setType('action');
+		$sarahCmd->setSubType('message');
+		$sarahCmd->save();
+	}
 
 }
 
 class sarahCmd extends cmd {
-    /*     * *************************Attributs****************************** */
+	/*     * *************************Attributs****************************** */
 
+	/*     * ***********************Methode static*************************** */
 
-    /*     * ***********************Methode static*************************** */
+	/*     * *********************Methode d'instance************************* */
 
+	public function dontRemoveCmd() {
+		return true;
+	}
 
-    /*     * *********************Methode d'instance************************* */
-
-    public function execute($_options = array()) {
-        if (!isset($_options['title']) && !isset($_options['message'])) {
-            throw new Exception(__("Le titre ou le message ne peuvent être tous les deux vide", __FILE__));
-        }
-        $eqLogic = sarah::byId($this->getEqLogic_id());
-        $message = '';
-        if (isset($_options['title']) && trim($_options['title']) != '') {
-            $message = $_options['title'] . '. ';
-        }
-        $message .= $_options['message'];
-        $http = new com_http($eqLogic->getConfiguration('addrSrvTts') . '/?tts=' . urlencode($message));
-        try {
-            return $http->exec();
-        } catch (Exception $e) {
-            if($eqLogic->getConfiguration('doNotThrowError',0) == 0){
-                throw $e;
-            }
-        }      
-    }
+	public function execute($_options = array()) {
+		if (!isset($_options['title']) && !isset($_options['message'])) {
+			throw new Exception(__("Le titre ou le message ne peuvent être tous les deux vide", __FILE__));
+		}
+		$eqLogic = sarah::byId($this->getEqLogic_id());
+		$message = '';
+		if (isset($_options['title']) && trim($_options['title']) != '') {
+			$message = $_options['title'] . '. ';
+		}
+		$message .= $_options['message'];
+		$http = new com_http($eqLogic->getConfiguration('addrSrvTts') . '/?tts=' . urlencode($message));
+		try {
+			return $http->exec();
+		} catch (Exception $e) {
+			if ($eqLogic->getConfiguration('doNotThrowError', 0) == 0) {
+				throw $e;
+			}
+		}
+	}
 
 }
